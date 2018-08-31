607cefcc90019b56cf88adaea618c24bc40c300a
Evgeny Savitsky <savickij.e@gmail.com>
2018-08-31 19:59:06 +0300

    second one

--- hiName/src/newProject.java
+++ hiName/src/newProject.java
@@ -2,4 +2,19 @@ public class newProject {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+    public static  void printArray (int[] a) {
+        for (int n : a) {
+            System.out.println(n);
+        }
+    }
+
+
+    public static int[] getRandomArray (int length) {
+        int[] numbers = new int[length];
+        for (int i = 0; i < length; i++) {
+            numbers[i] = (int) Math.random() * 10;
+        }
+        return numbers;
+    }
 }
